c45b5a9ddb057878d1fa80d97790e33165ec42eb
Piotr Purzycki <piotr@purzycki.pl>
2023-01-10 21:18:36 +0100

    dashboard widgets

--- src/App.js
+++ src/App.js
@@ -8,12 +8,11 @@ import Dashboard from './scenes/dashboard'
 import Team from './scenes/team'
 import Invoices from './scenes/invoices'
 import Contacts from './scenes/contacts'
-// import Bar from './scenes/bar'
+import Bar from './scenes/bar'
 import Form from './scenes/form'
-// import Line from './scenes/line'
-// import Pie from './scenes/pie'
-// import FAQ from './scenes/fAQ'
-// import Geography from './scenes/geography'
+import Line from './scenes/line'
+import Pie from './scenes/pie'
+import FAQ from './scenes/faq'
 import Calendar from './scenes/calendar'
 
 function App() {
@@ -31,12 +30,11 @@ function App() {
               <Route path="/team" element={<Team />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/contacts" element={<Contacts />} />
-              {/* <Route path="/bar" element={<Bar />} /> */}
+              <Route path="/bar" element={<Bar />} />
               <Route path="/form" element={<Form />} />
-              {/* <Route path="/line" element={<Line />} /> */}
-              {/* <Route path="/pie" element={<Pie />} /> */}
-              {/* <Route path="/faq" element={<FAQ />} /> */}
-              {/* <Route path="/geography" element={<Geography />} /> */}
+              <Route path="/line" element={<Line />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/faq" element={<FAQ />} />
               <Route path="/calendar" element={<Calendar />} />
             </Routes>
           </main>
--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -205,13 +205,6 @@ const Sidebar = () => {
               selected={selected}
               setSelected={setSelected}
             />
-            <Item
-              title="Geography Chart"
-              to="/geography"
-              icon={<MapOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
           </Box>
         </Menu>
       </ProSidebar>
