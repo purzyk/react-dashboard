2058b079066915766aad2a7f28df4fbe7864b4b5
Piotr Purzycki <piotr@purzycki.pl>
2023-01-10 11:57:56 +0100

    base setup, light / dark theme switch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-function App() {
-  return (
-    <div className="app">
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from './scenes/global/Topbar'
 
-    </div>
+function App() {
+  const [theme, colorMode] = useMode();
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
